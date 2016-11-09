51eceac8be7bcebb3a15e8c278bd321dafe9e102
rekked <roger.edwin@st.vvu.edu.gh>
2016-11-09 23:18:43 +0000

    nss_admin table created

--- nspmsdb.sql
+++ nspmsdb.sql
@@ -1,1 +1,11 @@
 #NSPMS DataBase
+
+CREATE TABLE nss_admin(
+nsid int(11) PRIMARY KEY AUTO_INCREMENT,
+fname varchar(85),
+mname varchar(85),
+lname varchar(85),
+userid varchar(85) UNIQUE,
+password varchar(85),
+tstamp DATETIME NULL
+);
