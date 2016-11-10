9b64b26908919ab69c94c7fd6373cbba221abd45
rekked <roger.edwin@st.vvu.edu.gh>
2016-11-10 02:24:54 +0000

    home page update

--- index.php
+++ index.php
@@ -1,1 +1,77 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="css/main.css">
+<link rel="stylesheet" href="css/font-awesome.css">
+</head>
 
+<body>
+<div id="runner">
+	<div id="contbx">
+		<div id="topbanner">
+			<div id="logobx"></div>
+			<div id="navbarbx">
+				<ul>
+					<li class="activetab">Home</li>
+					<li>News</li>
+					<li>Applicants</li>
+					<li>Companies</li>
+					<li>About</li>
+					<li>Contact</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+</div>
+<div id="sliderbx"></div>
+<div id="newsbx">
+	<div class="titlebar">
+		<span>News</span>
+	</div>
+	<div class="newscntbx">
+		<div class="newspicbx">
+			<img src="img/phold.png">
+		</div>
+		<div class="newstxtbx">News detail show below here with all ypou need to know...</div>
+	</div>
+	<div class="newscntbx">
+		<div class="newspicbx">
+			<img src="img/phold.png">
+		</div>
+		<div class="newstxtbx">News detail show below here with all ypou need to know...</div>
+	</div>
+	<div class="newscntbx">
+		<div class="newspicbx">
+			<img src="img/phold.png">
+		</div>
+		<div class="newstxtbx">News detail show below here with all ypou need to know...</div>
+	</div>
+	<div class="newscntbx">
+		<div class="newspicbx">
+			<img src="img/phold.png">
+		</div>
+		<div class="newstxtbx">News detail show below here with all ypou need to know...</div>
+	</div>
+</div>
+<div id="qicklnks">
+	<div class="qlinkbx">
+		<div class="qltopbx"><span class="linkicon regicon"></span></div>
+		<div class="qlinkname"><span>Enroll</span></div>
+	</div>
+	<div class="qlinkbx">
+		<div class="qltopbx"><span class="linkicon checkpost"></span></div>
+		<div class="qlinkname"><span>Postings</span></div>
+	</div>
+	<div class="qlinkbx">
+		<div class="qltopbx"><span class="linkicon company"></span></div>
+		<div class="qlinkname"><span>Sign up Company</span></div>
+	</div>
+	<div class="qlinkbx">
+		<div class="qltopbx"><span class="linkicon school"></span></div>
+		<div class="qlinkname"><span>Student List</span></div>
+	</div>
+
+</div>
+<div id="footercnt"></div>
+</body>
+</html>
