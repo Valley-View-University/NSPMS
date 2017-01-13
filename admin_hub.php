7e7899958c6357be28413cfbf8dc370bfb747db2
REK <reked@live.com>
2017-01-13 08:59:03 +0000

    company and admin module updated

--- admin_hub.php
+++ admin_hub.php
@@ -7,7 +7,93 @@ if(empty($_SESSION['aid']) == true) {
 	header("Location:admin.php");
 }
 
-$studgtq = mysqli_query($link, "");
+$year = date("Y",time());
+$studgtq = mysqli_query($link, "SELECT * FROM studentinfo WHERE year='$year' ");
+$stnum = mysqli_num_rows($studgtq);
+$studentax = '';
+
+if(isset($_POST['enroll'])){
+	$studentax = 0;
+	$alstq = mysqli_query($link, "SELECT * FROM studentinfo WHERE year='$year'");
+	$stnum = mysqli_num_rows($studgtq);
+	if($stnum > 0){
+		while ($sqr = mysqli_fetch_array($alstq, MYSQLI_ASSOC)) {
+			$sid = $sqr['sid'];
+			$gsrq = mysqli_query($link, "SELECT * FROM student_request WHERE sid='$sid' ");
+			$gr = mysqli_fetch_assoc($gsrq);
+			$option1 = $gr['option1'];
+			$option2 = $gr['option2'];
+			$option3 = $gr['option3'];
+
+			$region1 = $gr['region1'];
+			$region2 = $gr['region2'];
+			$region3 = $gr['region3'];
+
+			$offer = getpost($link, $region1, $option1);
+			if($offer != 0){
+				$pstq = mysqli_query($link, "INSERT INTO posting(`sid`,`cid`,`rid`,`oid`,`added`) VALUES('$sid','$offer','$region1','$option1',now()) ");
+				$studentax++;
+			}
+			else{
+				$offer = getpost($link, $region2, $option2);
+				if($offer != 0){
+					$pstq = mysqli_query($link, "INSERT INTO posting(`sid`,`cid`,`rid`,`oid`,`added`) VALUES('$sid','$offer','$region2','$option2',now()) ");
+					$studentax++;
+				}
+				else{
+					$offer = getpost($link, $region3, $option3);
+					if($offer != 0){
+						$pstq = mysqli_query($link, "INSERT INTO posting(`sid`,`cid`,`rid`,`oid`,`added`) VALUES('$sid','$offer','$region3','$option3',now()) ");
+						$studentax++;
+					}
+					else{
+						$offer = randpost($link);
+						if($offer != 0){
+							$offerhd = explode('|',$offer);
+							$cidx = $offerhd[0];
+							$oidx = $offerhd[1];
+
+							$gridq = mysqli_query($link, "SELECT rid FROM company WHERE cid='$cidx' ");
+							$rw = mysqli_fetch_assoc($gridq);
+							$rid = $rw['rid'];
+							$pstq = mysqli_query($link, "INSERT INTO posting(`sid`,`cid`,`rid`,`oid`,`added`) VALUES('$sid','$cidx','$rid','$oidx',now()) ");
+							$studentax++;
+						}
+						
+					}
+				}
+			}
+
+		}
+		$studentax = '<span style="color:#ccc;">Student(s) Posted </span>'.$studentax;
+	}
+}
+
+function getpost($link, $rid, $opid){
+	$ckpq = mysqli_query($link, "SELECT `cid` FROM company_request WHERE oid='$opid' AND cid IN (SELECT cid FROM company WHERE rid='$rid') ");
+	$pznum = mysqli_num_rows($ckpq);
+	if($pznum > 0){
+		$qrv = mysqli_fetch_assoc($ckpq);
+		return $qrv['cid'];
+	}
+	else{
+		return 0;
+	}
+
+}
+
+function randpost($link){
+	$ckpq = mysqli_query($link, "SELECT `cid`,`oid` FROM `company_request` WHERE number > posted ORDER BY RAND() LIMIT 1");
+	$pznum = mysqli_num_rows($ckpq);
+	if($pznum > 0){
+		$qrv = mysqli_fetch_assoc($ckpq);
+		return $qrv['cid'].'|'.$qrv['oid'];
+	}
+	else{
+		return 0;
+	}
+
+}	
 
 ?>
 
@@ -27,6 +113,11 @@ $studgtq = mysqli_query($link, "");
 		</div>
 	</div>
 </div>
-
+<span style="float:left;width:100%;margin:20px 0;font-size:18px;text-align:center;"><?php echo $studentax;?><br><span style="color:#ccc;">Student Enrolled this year:</span> <?php echo $stnum;?></span>
+<form method="POST" action="">
+	<div style="width: 300px;height: 20px;cursor:pointer;border:4px solid #ccc;border-radius: 5px;background-color: #8BC34A;margin: 100px auto;text-align:center;color:#fff;padding:20px;">
+		<button style="width:100%;height:100%;border:none;background:none;color:#fff;font-size:18px;" type="submit" name="enroll">Post enrolled students</button>
+	</div>
+</form>
 </body>
 </html>
--- company_hub.php
+++ company_hub.php
@@ -9,12 +9,51 @@ if (empty($_SESSION['cid']) == true) {
   header("Location:company.php");
 }
 
+$cid = $_SESSION['cid'];
+$year = date("Y",time());
+$done = '';
+if(isset($_POST['submit'])){
+  $delrq = mysqli_query($link, "DELETE FROM company_request WHERE cid='$cid' AND year='$year' ");
+
+  $optzq = mysqli_query($link, "SELECT * FROM options ORDER BY options ASC");
+  while ($opzr = mysqli_fetch_array($optzq, MYSQLI_ASSOC)) {
+    $oid = $opzr['oid'];
+    $oprion = clean($link, $_POST[$opzr['oid']]);
+    if($oprion >= 1){
+      $inq = mysqli_query($link, "INSERT INTO company_request(`cid`,`oid`,`number`,`added`,`year`) VALUES('$cid','$oid','$oprion',now(),'$year') ");
+    }
+  }
+  if($inq){
+    $done = '<span style="color:green;font-size:25px;width: 100%;text-align: center;float: left;">successful</span>';
+  }
+  else{
+    $done = '<span style="color:red;width:100%;font-size:25px;text-align: center;float: left;">error could not insert</span>';
+  }
+}
+
+
 $optionsx = '';
 $optq = mysqli_query($link, "SELECT * FROM options ORDER BY options ASC");
 while ($opr = mysqli_fetch_array($optq, MYSQLI_ASSOC)) {
-  $optionsx = '<div id="one">
+  $oidx = $opr['oid'];
+  $gtq = mysqli_query($link, "SELECT `number` FROM company_request WHERE cid='$cid' AND oid='$oidx' AND year='$year' ");
+  $gtnum = mysqli_num_rows($gtq);
+  if($gtnum == 0){
+    $valx = '';
+  }
+  else{
+    $grz = mysqli_fetch_assoc($gtq);
+    $valx = $grz['number'];
+  }
+
+  $optionsx .= '<div id="one">
+                  <div class="finfo">
+                      <div class="info">'.$opr['options'].'</div>
+                      <input type="number" style="width:50px;" value="'.$valx.'" class="field" name="'.$opr['oid'].'"/>
+                  </div>
                 </div>';
 }
+
 
       
      
@@ -24,6 +63,33 @@ while ($opr = mysqli_fetch_array($optq, MYSQLI_ASSOC)) {
 <head>
 <link rel="stylesheet" type="text/css" href="css/main.css">
 <link rel="stylesheet" href="css/font-awesome.css">
+<style type="text/css">
+  .info {
+    width: 248px;
+    height: 40px;
+    text-align: right;
+    float: left;
+    margin-top: 7px;
+    text-transform: capitalize;
+    padding-right: 5px;
+}
+
+#submit {
+    width: 300px;
+    height: 35px;
+    margin: 0 20px;
+    margin-bottom: 50px;
+    float: left;
+    font-family: "exo";
+    margin-top: 20px;
+    background-color: #adad44;
+    cursor: pointer;
+    border: 4px solid rgba(138, 115, 12, 0.31);
+    text-transform: capitalize;
+}
+
+</style>
+
 </head>
 
 <body>
@@ -35,13 +101,12 @@ while ($opr = mysqli_fetch_array($optq, MYSQLI_ASSOC)) {
     </div>
   </div>
 </div>
+<?php echo $done;?>
 <div id="main">
- <form method="POST" action="" enctype="multipart/form-data">
-  <div id="student">
-  <div id="one"><span id="log">Request Form</span></div>
+<div id="one"><span id="log">Request Form</span></div>
+ <form method="POST" action="" style="float:left;margin-left:450px;">
     <?php echo $optionsx;?>
     <div id="one"><button name="submit" id="submit"type="submit">submit</button></div>
-  </div>
  </form>
 </div>
 <div style="height:77px;float:left;" id="footercnt"></div> 
